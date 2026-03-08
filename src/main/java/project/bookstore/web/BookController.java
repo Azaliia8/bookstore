64c2ead23ea60c358ff23e8e036c88edef1250a7
Azaliia8 <azaliia.aitanova@gmail.com>
2026-03-08 17:20:16 +0200

    C3.3 CRUD

--- src/main/java/project/bookstore/web/BookController.java
+++ src/main/java/project/bookstore/web/BookController.java
@@ -2,9 +2,11 @@ package project.bookstore.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import project.bookstore.domain.Book;
 import project.bookstore.domain.BookRepository;
 
 @Controller
@@ -20,5 +22,23 @@ public class BookController {
   public String books(Model model) {
     model.addAttribute("books", repository.findAll());
     return "booklist";
+  }
+
+  @RequestMapping(value="/add")
+  public String addBook(Model model) {
+    model.addAttribute("book", new Book());
+    return "addbook";
+  }
+
+  @RequestMapping(value="/save", method=RequestMethod.POST) 
+  public String save(Book book) {
+    repository.save(book);
+    return "redirect:booklist";
+  }
+
+  @RequestMapping(value="/delete/{id}", method=RequestMethod.GET)
+  public String deleteBook(@PathVariable("id") Long bookId) {
+    repository.deleteById(bookId);
+    return "redirect:../booklist";
   }
 }
